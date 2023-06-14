788270ccab8b5823922acb939913b8edb8cc686b
Rasib0 <rasibnadeem101@gmail.com>
2023-06-14 11:17:27 +0500

    ran prettier

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,8 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-      publicRoutes: ["/"]
-  }
-);
+  publicRoutes: ["/"],
+});
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,20 +18,19 @@ const Home: NextPage = () => {
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-          
-          {!user && 
-          <SignInButton mode="modal">
-            <button className="btn">
-              Sign in
-            </button>
-          </SignInButton>}
-          {!!user && 
-          <SignOutButton>
-            <button className="btn">
-              Sign out
-            </button>
-            </SignOutButton>}
-          
+          {!user && (
+            <SignInButton mode="modal">
+              <button className="btn">Sign in</button>
+            </SignInButton>
+          )}
+          {!!user && (
+            <SignOutButton>
+              <button className="h-4 w-8 border-t-neutral-50 bg-red-200">
+                Sign out
+              </button>
+            </SignOutButton>
+          )}
+
           <p className="text-2xl text-white">
             {hello.data ? hello.data.greeting : "Loading tRPC query..."}
           </p>
